15c4882b0830214547d2a83e44658d14968dc0d8
Mohammed Thaha <expesion23exnoiu@gmail.com>
2021-06-05 11:25:51 +0530

    Create React App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return <div>hello</div>;
 }
 
 export default App;
